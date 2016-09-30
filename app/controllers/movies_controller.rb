9b3332e2cfea0197b17665d285cede530a2af26c
lokeshh <lokeshhsharma@gmail.com>
2016-09-30 06:03:17 +0000

    complete till flash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,8 @@
 class MoviesController < ApplicationController
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)  
+  end
+  
   def index
     @movies = Movie.all
   end
@@ -6,5 +10,14 @@ class MoviesController < ApplicationController
   def show
     id = params[:id]
     @movie = Movie.find(id)
+  end
+  
+  def new
+  end
+  
+  def create
+    @movie = Movie.create! movie_params
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
   end
 end
