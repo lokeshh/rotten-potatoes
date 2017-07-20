00a592ec3823167e2b4e868d01a152e56ab17ed5
lokeshh <lokeshhsharma@gmail.com>
2017-07-20 04:34:38 +0000

    reset application controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_filter :set_current_user
-  protected # prevents method from being invoked by a route
-  def set_current_user
-    # we exploit the fact that the below query may return nil
-    @current_user ||= Moviegoer.where(:id => session[:user_id]).first
-    redirect_to login_path and return unless @current_user
-  end
 end
